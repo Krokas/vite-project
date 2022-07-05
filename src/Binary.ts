7e0d2e2d2ebc3e0ceba00f48c9ee81e3e50fb068
Kondrackis, Rokas <rokas.kondrackis@teliacompany.com>
2022-07-05 13:38:25 +0300

    decimal to binary

--- src/Binary.ts
+++ src/Binary.ts
@@ -1,7 +1,7 @@
 class Binary {
   byteElement: HTMLDivElement;
-  decimalElement: HTMLDivElement;
-  byte: Boolean[] = new Array(8);
+  decimalElement: HTMLInputElement;
+  byte: boolean[] = new Array(8);
   decimal: Number = 0;
   constructor() {
     this.byteElement = document.createElement("div");
@@ -18,8 +18,14 @@ class Binary {
       );
       this.byteElement.append(checkbox);
     }
-    this.decimalElement = document.createElement("div");
-    this.decimalElement.innerHTML = this.decimal.toString();
+    this.decimalElement = document.createElement("input");
+    this.decimalElement.type = "number";
+    this.decimalElement.min = Number(0).toString();
+    this.decimalElement.max = Number(255).toString();
+    this.decimalElement.value = this.decimal.toString();
+    this.decimalElement.addEventListener("change", (event: Event) =>
+      this.handleDecimalChange(event)
+    );
     this.byteElement.classList.add("byte");
     this.decimalElement.classList.add("decimal");
   }
@@ -31,7 +37,35 @@ class Binary {
 
     this.byte[value] = isChecked;
     this.decimal = this.calculateDecimal();
-    this.decimalElement.innerHTML = this.decimal.toString();
+    this.decimalElement.value = this.decimal.toString();
+  }
+
+  handleDecimalChange(event: Event) {
+    const input: HTMLInputElement = event.target as HTMLInputElement;
+    let value: number = Number(input.value);
+
+    if (value > 255) {
+      value = 255;
+      input.value = value.toString();
+    }
+
+    if (value < 0) {
+      value = 0;
+      input.value = value.toString();
+    }
+    input.blur();
+
+    this.byte = this.calculateBinary(value);
+    const checkboxes: HTMLInputElement[] = Array.from(
+      this.byteElement.querySelectorAll("input[type=checkbox]")
+    );
+
+    this.byte.forEach((value: boolean, index: number) => {
+      const checkbox: HTMLInputElement | undefined = checkboxes.find(
+        (checkbox: HTMLInputElement) => checkbox.value === index.toString()
+      );
+      if (checkbox) checkbox.checked = value;
+    });
   }
 
   calculateDecimal(): number {
@@ -42,6 +76,18 @@ class Binary {
       }
     });
     return decimal;
+  }
+
+  calculateBinary(value: number): boolean[] {
+    let remainder: number = value;
+    let byte: boolean[] = Array(8);
+    for (let i = 7; i >= 0; i--) {
+      const divisor = Math.pow(2, i);
+      const bitValue = Math.floor(remainder / divisor);
+      remainder = remainder % divisor;
+      byte[i] = !!bitValue;
+    }
+    return byte;
   }
 
   getHTML(): HTMLElement {
